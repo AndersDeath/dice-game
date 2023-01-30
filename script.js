fdb31178962cfc4c4d98c0c028112a408189c0db
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-01-30 15:07:13 +0400

    update js logic

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+const numberPrefix = 'number-';
+const numberClasses = (() => {
+    let output = [];
+    for (let index = 1; index <= 6; index++) {
+        output.push(numberPrefix + index);
+    }
+    return output;
+})();
+
 const roll = () => {
     return Math.floor(Math.random() * 6) + 1;
 }
+
 const run = () => {
     let randomNumber1 = roll();
     let randomNumber2 = roll()
 
     console.log(randomNumber1);
     console.log(randomNumber2);
+}
 
+const setNumber = (el, num) => {
+    numberClasses.forEach((e) => {
+        el.classList.remove(e);
+    });
+    el.classList.add(numberPrefix + num)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+    document.querySelector('.setBox1').addEventListener('click', () => {
+        let num =roll();
+        console.log(num);
+        setNumber(document.querySelector('.box'), num);
+    })
 });
